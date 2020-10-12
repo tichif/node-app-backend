a3292132fddd81d26aa5083b0f8df2037f18ac05
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-10-11 20:16:29 -0400

    Handle errors

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const app = express()
 
 app.use('/api/places',placesRoutes)
 
+// Error handling middleware
+app.use((error,req, res, next) => {
+  if(res.headerSent){
+    return next(error)
+  }
+  res.status(error.code || 500)
+  .json({ message : error.message || 'An unknown error occurred !!!'})
+})
+
 app.listen(5000, () => {
   console.log("App is running");
 })
--- routes/places.js
+++ routes/places.js
@@ -18,16 +18,27 @@ const DUMMY_PLACES = [
 
 router.get('/user/:userId', (req, res) => {
   const userId = req.params.userId;
-  const places = DUMMY_PLACES.find(p => p.creator === userId)
-  res.json({ places })
+  const place = DUMMY_PLACES.find(p => p.creator === userId)
+  if(!place){
+    // if you use a synchronous code, you can use Throw
+    // if you use an asynchronous code, you use next()
+    const error = new Error("Can't find place for this user.")
+    error.code = 404;
+    throw error
+  }
+  res.json({ place })
 })
 
 
-router.get('/:placeId', (req, res) => {
+router.get('/:placeId', (req, res, next) => {
   const placeId = req.params.placeId;
   const place = DUMMY_PLACES.find(p => p.id === placeId)
+  if(!place){
+    const error = new Error("Can't find place for this id.")
+    error.code = 404;
+    return next(error)
+  }
   res.json({ place })
 });
-
 
 module.exports = router

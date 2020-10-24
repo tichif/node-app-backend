107b3f802ca83e61843373b60d756a1e63751fd6
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-10-23 23:11:29 -0400

    Install mongoose and create place model

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const HttpError = require('./models/http-error')
-const placesRoutes = require('./routes/places')
-const usersRoutes = require('./routes/users')
+const HttpError = require('./models/http-error');
+const placesRoutes = require('./routes/places');
+const usersRoutes = require('./routes/users');
 
-const app = express()
+const app = express();
 
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-app.use('/api/places',placesRoutes)
-app.use('/api/users', usersRoutes)
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 
 // Implement unsupported routes error
 app.use((req, res, next) => {
-  const error = new HttpError("Could not found this route", 404)
-  throw error
-})
+  const error = new HttpError('Could not found this route', 404);
+  throw error;
+});
 
 // Error handling middleware
-app.use((error,req, res, next) => {
-  if(res.headerSent){
-    return next(error)
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
   }
-  res.status(error.code || 500)
-  .json({ message : error.message || 'An unknown error occurred !!!'})
-})
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurred !!!' });
+});
 
-app.listen(5000, () => {
-  console.log("App is running");
-})
+mongoose
+  .connect(
+    'mongodb+srv://tichif:tichif@mern.uovut.mongodb.net/mern?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(5000, () => {
+      console.log('App is running');
+    });
+  })
+  .catch((err) => console.log(err));
